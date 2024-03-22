9a120228ad00725b647c87dfae8b931d266b5008
Wambaforestin <wambaforestin@gmail.com>
2024-03-22 10:45:10 +0100

    Completed the pricing section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// for the reviews section in the index.html file
 let slides = document.querySelectorAll('.reviews .flex .slide-container .slide');
 
 let currentSlide = 0;
@@ -13,4 +14,31 @@ function prevSlide() {
     slides[currentSlide].classList.remove('active');    
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     slides[currentSlide].classList.add('active');
+}
+
+//for the prcing section in the index.html file, changing the pricing plan
+
+let monthBtn = document.querySelector('.pricing .toggle-buttons .month-btn');
+
+let yearBtn = document.querySelector('.pricing .toggle-buttons .year-btn');
+
+let monthPlans = document.querySelectorAll('.pricing .box-container .month');
+
+let yearPlans = document.querySelectorAll('.pricing .box-container .year');
+
+
+yearBtn.onclick = function() {
+    monthBtn.classList.remove('active');
+    yearBtn.classList.add('active');
+
+    monthPlans.forEach(plans => {plans.style.display = 'none'});
+    yearPlans.forEach(plans => {plans.style.display = 'block'});
+}
+
+monthBtn.onclick = function() {
+    yearBtn.classList.remove('active');
+    monthBtn.classList.add('active');
+
+    yearPlans.forEach(plans => {plans.style.display = 'none'});
+    monthPlans.forEach(plans => {plans.style.display = 'block'});
 }
